49e00e46364fbd5151fe6cf7c3683d4d4a0de590
John Mackey <109514137+Sunday-Crunk@users.noreply.github.com>
2023-06-16 15:32:42 +0100

    Update MetadataImportChecksums.js

--- MetadataImportChecksums.js
+++ MetadataImportChecksums.js
@@ -552,6 +552,17 @@ function tagUploads(comparison, unloadedMatch, unloadedFail){
   var uploadedElements = Array.from(document.querySelectorAll(".upload-loaded"))
   console.log("comparison: ", comparison)
   comparison.matches.forEach(match => {
+    let pathLevels = match.Path.split("/")
+    if (pathLevels[1] !== match.Name){ //folder upload
+      pathLevels.shift()
+      var parent = uploadedElements
+      pathLevels.forEach(level =>{
+          parent = parent.find((element)=>{
+            element.textContent.includes(level)
+          })
+      })
+      console.log("parent: ", parent)
+    }
     const matchingDiv = uploadedElements.find((element) =>
       element.textContent.includes(match.Name)
     )?.querySelectorAll("div");
