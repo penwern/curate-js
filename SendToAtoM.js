3ec00704c9f7bc30748088e760cd46a29f7da147
John Mackey <109514137+Sunday-Crunk@users.noreply.github.com>
2024-03-01 15:45:12 +0000

    Update SendToAtoM.js

--- SendToAtoM.js
+++ SendToAtoM.js
@@ -1,11 +1,12 @@
-
-var token = PydioApi._PydioRestClient.authentications.oauth2.accessToken;
-var selectedNode = pydio._dataModel._selectedNodes[0];
-var path = selectedNode !== undefined && selectedNode !== null && selectedNode._path !== undefined ? "cellsdata/admin/atom" + selectedNode._path : "cellsdata/admin/atom" + pydio._dataModel._currentRep;
 function createPresignedDownloadUrl(path) {
-      return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         PydioApi.getRestClient().getOrUpdateJwt().then(jwt => {
-            import(/* webpackChunkName: 'aws-sdk' */ 'aws-sdk').then(aws => {
+            PydioApi._PydioClient.buildPresignedGetUrl(pydio._dataModel._selectedNodes[0]).then(e=>{
+                AWS.config.update({
+                    accessKeyId: 'gateway',
+                    secretAccessKey: 'gatewaysecret',
+                    s3ForcePathStyle: true
+                });
                 let params = {
                     Bucket: 'io',
                     Key: path,
@@ -13,91 +14,140 @@ function createPresignedDownloadUrl(path) {
                 };
                 const frontU = pydio.getFrontendUrl();
                 const url = `${frontU.protocol}//${frontU.host}`;
-                const s3 = new aws.S3({ endpoint: url });
+                const s3 = new AWS.S3({ endpoint: url });
                 const signed = s3.getSignedUrl('getObject', params);
                 const output = signed + '&pydio_jwt=' + jwt;
                 console.log("Generated URL:", output); // Log the generated URL
-                resolve(output); // Resolve the Promise with the generated URL
-                })
-            
-            }).catch(error => {
-                reject(error); // Reject the Promise if there's an error
-            });
+                resolve(output); // Resolve the Promise with the generated URL 
+            })
+        }).catch(error => {
+            reject(error); // Reject the Promise if there's an error
+        });  
     });
-    }
+}
+
 PydioApi.getRestClient().getOrUpdateJwt().then(jwt => {
-    fetch("https://www.curate.penwern.co.uk/a/tree/admin/list", {
-        "headers": {
-            "accept": "application/json",
-            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-            "authorization": "Bearer " + jwt,
-            "cache-control": "no-cache",
-            "content-type": "application/json",
-            "pragma": "no-cache",
-            "sec-ch-ua": "\"Not A(Brand\";v=\"99\", \"Google Chrome\";v=\"121\", \"Chromium\";v=\"121\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"Windows\"",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "x-pydio-language": "en-us"
-        },
-        "referrer": "https://www.curate.penwern.co.uk/settings/admin/audit",
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": JSON.stringify({
+    var paths = pydio._dataModel._selectedNodes.map(node => getOpenWS() + node._path);
+    var uploadData = {}; // Array to aggregate data from Promise.all
+    uploadData.Children = []
+    // Promises array to track all fetchCurate Promises
+    var fetchPromises = paths.map(path => {
+        return Curate.api.fetchCurate("/a/tree/admin/list", "POST", {
             Node: {
                 Path: path
             },
             Recursive: true
-        }),
-        "method": "POST",
-        "mode": "cors",
-        "credentials": "include"
-    }).then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json(); // This returns a Promise
-    }).then(data => {
-        delete data.Parent
-        Promise.all(data.Children.map(entry => {
-            if (entry.Type === "LEAF") {
-                return createPresignedDownloadUrl(entry.Path.replace("cellsdata/admin/", ""))
+        }).then(response => {
+            if (!response.status == 200) {
+                throw new Error('Network response was not ok');
+            }
+            return response;
+        }).then(data => {
+            if (!data.Children) {
+                window.alert("Record " + path + " contains no children");
+                return;
+            }
+
+            var parentRef = JSON.parse(data.Parent.MetaStore["usermeta-isadg-reference-codes"]);
+            var parentUuid = data.Parent.Uuid;
+            delete data.Parent;
+
+            // Promise.all to generate presigned URLs for all children
+            return Promise.all(data.Children.map(entry => {
+                return createPresignedDownloadUrl(entry.Path)
                     .then(downloadUrl => {
                         entry.DownloadUrl = downloadUrl;
-                        console.log("Download URL for", entry.Path, ":", downloadUrl);
+                        entry.ParentRef = parentRef;
+                        entry.ParentUuid = parentUuid;
                     })
                     .catch(error => {
                         console.error('Error generating download URL:', error);
                     });
-            }
-        })).then(() => {
-            // All promises resolved, now proceed with the final fetch call
-            console.log("All download URLs generated, sending data...");
-            console.log(data);
-            fetch("http://127.0.0.1:5000/api/atom_connector/upload", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            }).then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            }).then(data => {
-                console.log(data);
-                //discover success of operation and tag records accordingly
-            }).catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
+            })).then(() => {
+                // Push data to uploadData array
+                data.Children.forEach(c=>uploadData.Children.push(c))
             });
         }).catch(error => {
-            // Handle errors from any of the promises
-            console.error('There was a problem:', error);
+            console.error('There was a problem with the fetch operation:', error);
+        });
+    });
+
+    // Execute all fetchPromises
+    Promise.all(fetchPromises).then(() => {
+        console.log("All download URLs generated, sending data...");
+        console.log(uploadData); // Array of data to be uploaded
+            var parentMap = pydio._dataModel._selectedNodes.map(node => node._metadata.get('uuid'));
+           var propMap = parentMap.map(uuid => ({
+                    NodeUuid: uuid,
+                    JsonValue: JSON.stringify("connecting"),
+                    Namespace: "usermeta-atom-connection",
+                    Policies: [{
+                            "Action": "READ",
+                            "Effect": "allow",
+                            "Subject": "*"
+                        },
+                        {
+                            "Action": "WRITE",
+                            "Effect": "allow",
+                            "Subject": "*"
+                        }
+                    ]
+                }));
+    
+                // Update meta data
+                Curate.api.fetchCurate("/a/user-meta/update", "PUT", {
+                    MetaDatas: propMap,
+                    Operation: "PUT"
+                }).then(response => {
+                    console.log(response);
+                })
+        // Send aggregated data in one batch to atom connector upload endpoint
+        fetch("http://127.0.0.1:5000/api/atom_connector/upload", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(uploadData)
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        }).then(data => {
+            // Handle response
+            console.log(data);
+            
+            // Prepare propMap for update
+            var propMap = data.completed.map(uuid => ({
+                NodeUuid: uuid,
+                JsonValue: JSON.stringify("connected"),
+                Namespace: "usermeta-atom-connection",
+                Policies: [{
+                        "Action": "READ",
+                        "Effect": "allow",
+                        "Subject": "*"
+                    },
+                    {
+                        "Action": "WRITE",
+                        "Effect": "allow",
+                        "Subject": "*"
+                    }
+                ]
+            }));
+
+            // Update meta data
+            Curate.api.fetchCurate("/a/user-meta/update", "PUT", {
+                MetaDatas: propMap,
+                Operation: "PUT"
+            }).then(response => {
+                console.log(response);
+            }).catch(error => {
+                console.error('There was a problem updating meta data:', error);
+            });
+        }).catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
         });
     }).catch(error => {
-        // Handle errors here
         console.error('There was a problem with the fetch operation:', error);
     });
 });
