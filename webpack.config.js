f94c1142658763fbb1a504ca9226babc264fa708
John Mackey <john@penwern.co.uk>
2024-03-26 14:47:18 +0000

    change to minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+  mode: 'production', // Set mode to production
   entry: './src/js/index.js', // Assuming you have an index.js file as the entry point for your JavaScript
   output: {
     filename: 'curate_bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    minimize: true, // Enable minimization
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_fnames: true // Exclude function names from being mangled
+          // Add any other options you need
+        }
+      })
+    ]
   },
   module: {
     rules: [
