fa528c2df5f06a25625635fa995decdca7b2f25d
John Mackey <john@penwern.co.uk>
2024-03-26 20:30:22 +0000

    added load event to trigger external scripts

--- src/js/external/ExternalScripts.js
+++ src/js/external/ExternalScripts.js
@@ -1,4 +1,4 @@
-const scripts = [
+const externalScripts = [
     {
         "name":"he",
         "url":"https://cdn.jsdelivr.net/npm/he@1.2.0/he.min.js"
@@ -20,21 +20,25 @@ const scripts = [
         "url":"https://cdnjs.cloudflare.com/ajax/libs/spark-md5/3.0.2/spark-md5.min.js"
     }
 ]
-
-scripts.forEach(script=>{
-    // Create a <script> element
-    let s = document.createElement('script');
-
-    // Set the source attribute to the CDN URL of the library
-    s.src = script.url
-
-    // Define a callback function to execute once the script is loaded
-    s.onload = function() {
-        // Code to execute after the library is loaded
-        console.log('Loaded external library: ', script.name);
-    };
-
-    // Append the <script> element to the <head> section of the document
-    document.head.appendChild(s);
-
+function applyScripts(scripts){
+    scripts.forEach(script=>{
+        // Create a <script> element
+        let s = document.createElement('script');
+    
+        // Set the source attribute to the CDN URL of the library
+        s.src = script.url
+    
+        // Define a callback function to execute once the script is loaded
+        s.onload = function() {
+            // Code to execute after the library is loaded
+            console.log('Loaded external library: ', script.name);
+        };
+    
+        // Append the <script> element to the <head> section of the document
+        document.head.appendChild(s);
+    
+    })
+}
+document.addEventListener("DOMContentLoaded", e=>{
+    applyScripts(externalScripts)
 })
