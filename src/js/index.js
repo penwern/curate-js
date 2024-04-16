872dc2af766a61ea5989f3c89ce4b8e64d6fb395
John Mackey <john@penwern.co.uk>
2024-04-16 14:06:59 +0100

    deprecated metadatapanel changes, added new css

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import '../js/core/CurateFunctions/CurateFunctions.js';
 import '../js/core/CustomPreservationConfigs.js';
-//import './deprecated/EnterpriseMetadataPanel_V2.js';
 import '../js/core/fileInfoModifier.js';
 import '../js/core/MetadataHierarchies.js';
 import '../js/core/OAIHarvestClient.js';
@@ -9,8 +8,6 @@ import '../js/core/tourModifier.js';
 //import './deprecated/UploadChecksumGenerator.js';
 import '../js/core/PermissionEnforcers.js'
 import '../js/external/ExternalScripts.js';
-//import '../js/templates/CurateMetadataPanelTemplate.js'
-
 import 'chart.js';
 import Swal from 'sweetalert2';
 import Papa from 'papaparse';
