c406ba6f68659edd16259c749f960f4557b2d493
John Mackey <john@penwern.co.uk>
2024-07-03 11:27:40 +0100

    upload error modal

--- src/js/core/PermissionEnforcers.js
+++ src/js/core/PermissionEnforcers.js
@@ -6,16 +6,16 @@ const permissionHandlers = {
             description: "enforce workspace upload permissions for standard users",
             handler: (e)=>{
                 if (!['quarantine', 'personal-files', 'common files'].includes(Curate.workspaces.getOpenWorkspace()) && pydio.user.idmUser.Roles.find(r=>r.Label = "Standard User")){
-                    Curate.ui.modals.curatePopup.fire({afterLoaded:(container)=>{
+                    e.stopImmediatePropagation()
+                    Curate.ui.modals.curatePopup({"title": "You do not have permission to upload to this workspace"},{afterLoaded:(container)=>{
                         container.innerHTML = `<div class="config-modal-content">
                             <div class="config-popup-title">
-                                <h2>You do not have permission to upload to this workspace.</h2>
                                 <p>Please upload your content to the Quarantine workspace instead.</p>
                             </div>
                         </div>`;
-                    }})
-                    e.stopImmediatePropagation()
-            }}
+                    }}).fire() 
+                }
+            }
         }  
     }
 }
