e2b6e0927248788caee545614f42246a33095d93
John Mackey <john@penwern.co.uk>
2024-07-03 17:05:26 +0100

    link to slug

--- src/js/web-components/atom-search.js
+++ src/js/web-components/atom-search.js
@@ -542,6 +542,7 @@ class AtoMSearchInterface extends HTMLElement {
                     <h4>${result.title}</h4>
                     <p>Reference code: ${result.reference_code}</p>
                     <p>Level of description: ${result.level_of_description}</p>
+                    <p>URL: ${this.atomUrl}/${result.slug}</p>
                     <button type="button" class="button" onclick="this.getRootNode().host.handleResultClick('${result.slug}')">Link to this result</button>
                   </div>
                 ${result.thumbnail_url ? 
