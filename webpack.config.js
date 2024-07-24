d071aa2be16a42cf01259284778ea888d0d1adc5
John Mackey <john@penwern.co.uk>
2024-07-24 14:58:59 +0100

    removed bundled vendor packages, added code splitting, implemented bundle versioning

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,54 @@
 const path = require('path');
+const webpack = require('webpack');
+const packageJson = require('./package.json');
+const TerserPlugin = require('terser-webpack-plugin');
+//const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
-  entry: './src/js/index.js', // Assuming you have an index.js file as the entry point for your JavaScript
+  entry: './src/js/index.js',
   output: {
-    filename: 'curate_bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: `[name]_${packageJson.version}.js`,
+    path: path.resolve(__dirname, `dist/${packageJson.version}`),  // Version-specific output directory
+    chunkFilename: '[name].[chunkhash].js',
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.VERSION': JSON.stringify(packageJson.version),
+    }),
+    //new BundleAnalyzerPlugin(),  // Include the bundle analyzer plugin
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         include: [
           path.resolve(__dirname, 'src/js/core'),
-          path.resolve(__dirname, 'src/js/core/CurateFunctions'),
-          path.resolve(__dirname, 'src/js/external'),
+          //path.resolve(__dirname, 'src/js/external'),
           path.resolve(__dirname, 'src/js/workers'),
           path.resolve(__dirname, 'src/js/templates'),
           path.resolve(__dirname, 'src/js/web-components'),
         ],
-        exclude: [
-          path.resolve(__dirname, 'src/js/core/CurateFunctions/CurateFunctions.js'),
-        ],
-        use: 'babel-loader' // Add any other loaders you need for JavaScript files
+        use: 'babel-loader'
       }
     ]
-  }
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: 'all',
+      maxSize: 200000,  // Split chunks larger than 200KB
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+        default: {
+          minChunks: 2,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
 };
