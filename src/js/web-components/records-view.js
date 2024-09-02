689dd2eebc3848096f3ebdde92d191b0cc7abef3
John Mackey <john@penwern.co.uk>
2024-09-02 16:45:26 +0100

    hm

--- src/js/web-components/records-view.js
+++ src/js/web-components/records-view.js
@@ -1,6 +1,14 @@
-
+import { AirComponent, html, state, createQuery } from "@air-apps/air-js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log('Loading records-view.js');
+    AirComponent("records-view", props => {
+        
+        return html`
+            <div class="records-view">
+                <h1>Records View</h1>
+            </div>
+        `;
+    })
 });
