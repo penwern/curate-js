0fb62b84e4728e2e2a6e38ada2ba5d33ef9cab3c
John Mackey <john@penwern.co.uk>
2024-09-02 16:30:46 +0100

    simplified records view

--- src/js/web-components/records-view.js
+++ src/js/web-components/records-view.js
@@ -1,36 +1,11 @@
 import { AirComponent, html, state, createQuery } from "@air-apps/air-js";
-console.log("heheheheheheheeheheheh")
-const rf = async (nodePaths, limit, offset) => {
-    const response = fetch(`/api/a/tree/stats`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          NodePaths: nodePaths,
-          Limit: limit,
-          Offset: offset,
-          AllMetaProviders: true,
-        }),
-    });
-      
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-      
-    return response.json();
-}
 
 
-
-const RecordsView = AirComponent("records-view", props => {
-    const { isLoading, error, data } = createQuery('records', rf(['appraisal/*'], 10, 0));
+AirComponent("records-view", props => {
+    
     return html`
         <div class="records-view">
             <h1>Records View</h1>
-            ${isLoading ? html`<div>Loading...</div>` : null}
-            ${error ? html`<div>Error: ${error}</div>` : null}
-            ${data ? html`<div>Data: ${JSON.stringify(data)}</div>` : null}
         </div>
     `;
 })
