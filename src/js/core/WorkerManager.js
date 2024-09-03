4d163d9801996cd7561f380a0fe260c88c3ed3bc
John Mackey <john@penwern.co.uk>
2024-09-03 15:17:00 +0100

    inline worker

--- src/js/core/WorkerManager.js
+++ src/js/core/WorkerManager.js
@@ -10,22 +10,67 @@ class CurateWorkerManager {
     }
 
     initWorker() {
-        fetch(this.workerScriptUrl)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Failed to load worker script.');
+        const scriptContent = `
+            importScripts("https://cdnjs.cloudflare.com/ajax/libs/spark-md5/3.0.2/spark-md5.min.js")
+
+            const incrementalMD5 = file => new Promise((resolve, reject) => {
+            var loaded = 0;
+            var startTime = performance.now();
+            var tSize = file.size;
+            const fileReader = new FileReader();
+            const spark = new SparkMD5.ArrayBuffer();
+            const chunkSize = 2097152; // Read in chunks of 2MB
+            const chunks = Math.ceil(file.size / chunkSize);
+            let currentChunk = 0;
+            console.log("start")
+            fileReader.onload = event => {
+                console.log("loaded")
+                spark.append(event.target.result); // Append array buffer
+                ++currentChunk;
+                if (currentChunk < chunks) {
+                    loadNext();
+                } else {
+                    resolve(spark.end()); // Compute hash
                 }
-                return response.text();
-            })
-            .then(scriptContent => {
-                const blob = new Blob([scriptContent], { type: 'application/javascript' });
-                const blobURL = URL.createObjectURL(blob);
-                this.worker = new Worker(blobURL);
-                this.setupWorkerHandlers();
-            })
-            .catch(error => {
-                console.error('Worker initialization failed:', error);
+            };
+
+            fileReader.addEventListener("progress", event => {
+                loaded += event.loaded;
+                let pE = Math.round((loaded / tSize) * 100);
+                let rS = pE + "%";
+                // console.log(rS)
             });
+
+            fileReader.addEventListener("loadend", event => {
+                if (event.total > 0) {
+                    var endTime = performance.now();
+                }
+            });
+
+            fileReader.onerror = () => reject(fileReader.error);
+
+            const loadNext = () => {
+                const start = currentChunk * chunkSize;
+                const end = start + chunkSize >= file.size ? file.size : start + chunkSize;
+                fileReader.readAsArrayBuffer(File.prototype.slice.call(file, start, end));
+            };
+
+            loadNext();
+            });
+
+            self.onmessage = async function(event) {
+            if (event.data.file && (event.data.msg == "begin hash")) {
+                const gmd5 = await incrementalMD5(event.data.file);
+                postMessage({ status: "complete", hash: gmd5 });
+                // when finished, close the worker
+                self.close();
+            }
+            };
+        `;
+        const blob = new Blob([scriptContent], { type: 'application/javascript' });
+        const blobURL = URL.createObjectURL(blob);
+        this.worker = new Worker(blobURL);
+        this.setupWorkerHandlers();
     }
 
     setupWorkerHandlers() {
