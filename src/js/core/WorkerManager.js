0e851bf75a33fed6a0873ca9e4bad2f88af03389
John Mackey <john@penwern.co.uk>
2024-09-04 11:40:36 +0100

    direct chuunk import

--- src/js/core/WorkerManager.js
+++ src/js/core/WorkerManager.js
@@ -1,29 +1,41 @@
-//import SparkMD5 from 'spark-md5';
-
 class CurateWorkerManager {
     constructor() {
         this.taskQueue = [];
         this.isProcessing = false;
-        this.SparkMD5 = this.SparkMD5 || SparkMD5;
-        console.log('CurateManager constructed');
+        this.worker = null;
     }
 
-    async init() {
-        if (!this.SparkMD5) {
-            try {
-                //const SparkMD5Module = await import(/* webpackChunkName: "spark-md5" */ 'spark-md5');
-                //this.SparkMD5 = SparkMD5Module.default || SparkMD5Module;
-                console.log('SparkMD5 loaded successfully');
-            } catch (error) {
-                console.error('Failed to load SparkMD5:', error);
-                throw error;
-            }
+    initWorker() {
+        if (this.worker) {
+            this.worker.terminate();
         }
+        
+        // Load the worker from jsDelivr
+        const workerUrl = './workers/hashWorker.worker.js';
+        this.worker = new Worker(workerUrl);
+        this.setupWorkerHandlers();
+    }
+    setupWorkerHandlers() {
+        this.worker.onmessage = event => {
+            if (event.data.status === "complete" && this.currentResolve) {
+                this.currentResolve({
+                    file: this.currentFile,
+                    hash: event.data.hash,
+                    name: this.currentFile.name
+                });
+            }
+            this.processNextTask();
+        };
+
+        this.worker.onerror = event => {
+            if (this.currentReject) {
+                this.currentReject('Worker error: ' + event.message);
+            }
+            this.processNextTask();
+        };
     }
 
-    async generateChecksum(file) {
-        console.log('generateChecksum called with file:', file.name);
-        await this.init();
+    generateChecksum(file) {
         return new Promise((resolve, reject) => {
             this.taskQueue.push({ file, resolve, reject });
             if (!this.isProcessing) {
@@ -34,59 +46,22 @@ class CurateWorkerManager {
 
     processNextTask() {
         if (this.taskQueue.length > 0) {
+            if (!this.worker) {
+                this.initWorker();
+            }
             const task = this.taskQueue.shift();
+            this.currentResolve = task.resolve;
+            this.currentReject = task.reject;
+            this.currentFile = task.file;
             this.isProcessing = true;
-            this.processFile(task.file)
-                .then(hash => {
-                    task.resolve({
-                        file: task.file,
-                        hash: hash,
-                        name: task.file.name
-                    });
-                    this.processNextTask();
-                })
-                .catch(error => {
-                    task.reject('Checksum error: ' + error.message);
-                    this.processNextTask();
-                });
+            this.worker.postMessage({ file: task.file, msg: "begin hash" });
         } else {
             this.isProcessing = false;
+            if (this.worker) {
+                this.worker.terminate();
+                this.worker = null;
+            }
         }
-    }
-
-    processFile(file) {
-        return new Promise((resolve, reject) => {
-            const chunkSize = 2097152; // Read in chunks of 2MB
-            const spark = new SparkMD5.ArrayBuffer();
-            const fileReader = new FileReader();
-
-            let currentChunk = 0;
-            const loadNext = () => {
-                const start = currentChunk * chunkSize;
-                const end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
-
-                fileReader.onload = (e) => {
-                    spark.append(e.target.result); // Append array buffer
-                    currentChunk++;
-
-                    if (currentChunk * chunkSize < file.size) {
-                        loadNext();
-                    } else {
-                        const hash = spark.end();
-                        resolve(hash);
-                    }
-                };
-
-                fileReader.onerror = (error) => {
-                    reject(error);
-                };
-
-                const slice = file.slice(start, end);
-                fileReader.readAsArrayBuffer(slice);
-            };
-
-            loadNext();
-        });
     }
 }
 
